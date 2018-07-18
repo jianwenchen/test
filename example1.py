2d4994d10e087264534f980b820ee7eb52bc994f
jianwenchen <chenjianwen@hit.edu.cn>
2018-07-18 02:20:38 +0000

    something new added

--- example1.py
+++ example1.py
@@ -1,1 +1,3 @@
 print("This is another program")
+print("add something new")
+
--- example2.py
+++ example2.py
@@ -1,1 +1,6 @@
 print("This is an example from the ec2")
+sum = 0
+for i in range(1, 101):
+    sum += i
+print(sum)
+
